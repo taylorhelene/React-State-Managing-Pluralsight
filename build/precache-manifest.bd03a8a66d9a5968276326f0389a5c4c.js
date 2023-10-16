dc1e82200716f5c90248cbc0eb7a9f7fa14801f5
taylorhelene <taylorhelene09@gmail.com>
2023-10-16 11:27:54 +0000

    change

---
+++ build/precache-manifest.bd03a8a66d9a5968276326f0389a5c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9583a1d19c9e063d6100c3ed9b041dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad2e2711f0395b8d5ff5",
+    "url": "/static/css/main.4ecbd5e7.chunk.css"
+  },
+  {
+    "revision": "7077702e87b7cc745e62",
+    "url": "/static/js/2.b02c9c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b02c9c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2e2711f0395b8d5ff5",
+    "url": "/static/js/main.c27849f2.chunk.js"
+  },
+  {
+    "revision": "150ed36e387c082d59de",
+    "url": "/static/js/runtime-main.f762ba71.js"
+  }
+]);
